b633ca9e47bc17975afec1ecb9b358f124006a94
bulat <b.islamov@tugush.com>
2019-01-06 19:02:24 +0300

    First commit

---
+++ 04/fill/Fill.asm
@@ -0,0 +1,118 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// Put your code here.
+
+// if (board != 0) than M[0] = 1
+// 	else M[0] = 0
+
+
+(LOOP)
+@R13
+M=1
+@BLACK_SCREEN
+0;JMP
+
+(SET_ONE)
+@R5
+M=1
+@R14
+M=-1
+@R13
+M=1
+@BLACK_SCREEN
+0;JMP
+
+
+//start of black screen
+//@R14 which color; @R13 where to return
+(BLACK_SCREEN)
+    
+    @256
+    D=A
+    @i
+    M=D
+    
+    @index
+    M=0
+
+    (EXTERNAL_LOOP)
+	// if(i==0) exit loop
+	@i
+	D=M
+	@END_EXT_LOOP
+	D;JEQ
+
+	    //payload
+	    @32
+	    D=A
+	    @j
+	    M=D
+	    (INNER_LOOP)
+
+    	        //if (j==0) goto END_INNER_LOOP
+		@j
+		D=M
+		@END_INNER_LOOP
+		D;JEQ
+		
+		//payload. Count index. Save to @temp. Retreive filling value.
+		@index
+		D=M
+		@SCREEN
+		A=A+D
+		D=A
+		@temp
+		M=D
+		
+		@KBD
+		D=M
+		@SET_ONE
+		D;JNE
+		@R14
+		M=0
+		@END_SET
+		0;JMP
+
+		(SET_ONE)
+		@R14
+		M=-1
+		(END_SET)
+
+		@R14
+		D=M
+		@temp
+		A=M
+		M=D
+
+		//j=j-1; index++
+		@j
+		M=M-1
+		@index
+		M=M+1
+		
+		@INNER_LOOP
+		0;JMP
+	    (END_INNER_LOOP)
+
+	//i = i-1; goto ext_loop
+	@i
+	M=M-1
+	@EXTERNAL_LOOP
+	0;JMP
+
+(END_EXT_LOOP)
+//return to calling code
+@R13
+A=M
+0;JMP
---
+++ 04/mult/mult.asm
@@ -0,0 +1,57 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+@R0
+D=M
+@first //@16
+M=D
+
+@R1
+D=M
+@second //@17
+M=D
+
+@sum //@18
+M=0
+
+(LOOP)
+@second //if second <= 0 than goto end
+D=M
+@STORE_R
+D;JLE
+
+@sum //sum += first
+D=M
+@first
+D=D+M
+@sum
+M=D
+
+@second //second -= 1
+M=M-1
+
+@LOOP
+0;JMP
+
+    (STORE_R)
+@sum
+D=M
+@R2
+M=D
+
+(END)
+@END
+0;JMP
+
+//if (second>0) {
+//    sum += first
+//    second -= 1
+//} else {
+//    R2=sum
+//}
